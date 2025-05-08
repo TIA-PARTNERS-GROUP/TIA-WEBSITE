be35091216073ff7aac7a4370ea47febd47252a2
SegusFaultise <wilsonzakk8@gmail.com>
2025-05-08 16:52:49 +1000

    Added jest testing for the backend

---
+++ tia_partners_backend/index.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('./index');
+
+describe('GET /api', () => {
+  it('should return hello message', async () => {
+    const res = await request(app).get('/api');
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('Hello from the backend!');
+  });
+});
