950f97b52dca76db6470770fba6887e4ad762ec5
SegusFaultise <wilsonzakk8@gmail.com>
2025-06-10 04:24:00 +1000

    Cleaned up folder structure

--- tia_partners_frontend/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom', 
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleFileExtensions: ['js', 'jsx'],
-};
--- tia_partners_frontend/src/routes/LazyPages.js
+++
@@ -1,5 +0,0 @@
-// src/routes/lazyPages.js
-export const lazyImports = {
-  '/benefits': () => import('../pages/Benefits/BenefitsPage.jsx'),
-  '/membership': () => import('../pages/Membership/MembershipPage.jsx'),
-};
