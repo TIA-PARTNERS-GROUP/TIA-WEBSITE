bbd27fd7a0e054f78d6fe104cc653f7c4e6e7af0
FocusedLoop <joshuawlod2003@gmail.com>
2025-07-29 16:11:46 +1000

    slite tweak with axios api testing

--- Frontend/src/api/backendAPICallExample.js
+++ Frontend/src/api/backendAPICallExample.js
@@ -2,7 +2,7 @@ import api from './axios';
 
 export const getBackendAPI = async (formdata) => {
     try {
-        const res = await api.get('/test');
+        const res = await api.get('/');
         return res.data;
     } catch (error) {
         console.error('Test call failed:', error);
