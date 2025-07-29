183818b526a97add55bacf97f8338b3bc688896c
Liam Atkinson <n11342901@qut.edu.au>
2025-07-29 20:30:06 +1000

    QuickSearch data update

--- Frontend/src/components/Portal/Connect/QuickSearch.jsx
+++ Frontend/src/components/Portal/Connect/QuickSearch.jsx
@@ -1,11 +1,40 @@
 import ConnectionsGrid from "../../../components/Portal/Connect/ConnectionsGrid";
 
 const connectionsData = [
-  {title: "DexRouter Ltd.", description: "Here at DexRouter, we manufacture and distribute cutting-edge router products."},
-  {title: "Jim's Cabling", description: "Providing all of your cabling needs!"},
-  {title: "Westmine Solutions", description: "Westmine Solutions is a bustling startup focusing on providing high-quality web development solutions."},
-  {title: "AccuT", description: "Fast, reliable, and always on time."}
-]
+    {
+        title: "DexRouter Ltd.", 
+        description: "Here at DexRouter, we manufacture and distribute cutting-edge router products.", 
+        whatwedoData: [
+            {description: "Router Manufacturing"}
+        ],
+        contactInfo: ["Tim Matters", "0123 456 789", "tim@dexrouter.com.au"]
+    },
+    {
+        title: "Jim's Cabling", 
+        description: "Providing all of your cabling needs!",
+        whatwedoData: [
+            {description: "Cabling Installation"}
+        ],
+        contactInfo: ["Jim Bean", "0123 456 789", "jim@jimscabling.com.au"]
+    },
+    {
+        title: "Westmine Solutions", 
+        description: "Westmine Solutions is a bustling startup focusing on providing high-quality web development solutions.",
+        whatwedoData: [
+            {description: "Website creation"},
+            {description: "Custom LLM creation"}
+        ],
+        contactInfo: ["Anton Kirkegard", "0123 456 789", "anton@dwestminesolutions.com.au"]
+    },
+    {
+        title: "AccuT", 
+        description: "Fast, reliable, and always on time.",
+        whatwedoData: [
+            {description: "Package Delivery"}
+        ],
+        contactInfo: ["Franz Kafka", "0123 456 789", "franz@accut.com.au"]
+    }
+];
 
 const QuickSearch = () => (
   <div className="container mx-auto flex flex-col items-start px-0 py-4 text-left">
