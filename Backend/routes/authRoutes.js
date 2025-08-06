50ac29d6d6ad09bb49800b146cd327945cf5e983
unknown <williamcarter808@gmail.com>
2025-08-06 15:06:10 +1000

    Added swagger schema for login route

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import Router from 'express';
 import { signup, verifyEmail, resendVerification, login, logout, forgotPassword, resetPassword } from '../controllers/authController.js'
+import { verifyToken } from '../middleware/authTolkien.js'
 
 const router = Router();
 
@@ -71,15 +72,68 @@ const router = Router();
  *       409:
  *         description: Requested email is already in use by another user
  */
+
+
 router.post('/signup', signup);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: User login
+ *     tags:
+ *         - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: mysecretpassword
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Login successful
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *       401:
+ *         description: Invalid email or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Invalid email or password
+ */
+router.post('/login', login);
+
+router.post('/logout', logout);
 
 router.post('/verifyEmail', verifyEmail);
 
 router.post('/resendVerification', resendVerification);
 
-router.post('/login', login);
-
-router.post('/logout', logout);
 
 router.post('/forgotPassword', forgotPassword);
 
